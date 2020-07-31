36cedea87b93d7d5effeefcbff1fa7fe05f02c64
Jeremy Drake <github@jdrake.com>
2020-07-31 11:24:04 -0700

    Initial commit.
    Workflow to build MSYS2 packages for i686.

---
+++ build32.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+source 'ci-library.sh'
+cd r
+mkdir ../artifacts
+git_config user.email 'ci@msys2.org'
+git_config user.name  'MSYS2 Continuous Integration'
+packages=( "$@" )
+
+# Build
+message 'Building packages' "${packages[@]}"
+execute 'Approving recipe quality' check_recipe_quality
+for package in "${packages[@]}"; do
+    execute 'Building binary' makepkg --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
+    execute 'Building source' makepkg --noconfirm --noprogressbar --skippgpcheck --allsource
+    grep -qFx "$(<../dont-install-list.txt)" <<< "${package}" || execute 'Installing' yes:pacman --noprogressbar --upgrade *.pkg.tar.*
+    execute 'Checking dll depencencies' list_dll_deps ./pkg
+    mv "${package}"/*.pkg.tar.* ../artifacts
+    mv "${package}"/*.src.tar.gz ../artifacts
+    unset package
+done
+
+# Deploy
+cd ../artifacts
+execute 'SHA-256 checksums' sha256sum *
+success 'All artifacts built successfully'
