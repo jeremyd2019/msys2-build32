c07c6e80a8b3c9f442cb7f0afe298c1034170ea7
jeremyd2019 <github@jdrake.com>
2022-05-10 13:45:33 -0700

    source packages have been switched to .tar.zst

--- build32.sh
+++ build32.sh
@@ -25,7 +25,7 @@ for package in "${packages[@]}"; do
     grep -qFx "${package}" ../ci-dont-install-list.txt || execute 'Installing' yes:pacman --noprogressbar --noconfirm --upgrade *.pkg.tar.*
     execute 'Checking dll depencencies' list_dll_deps ./pkg
     mv "${package}"/*.pkg.tar.* ../artifacts
-    mv "${package}"/*.src.tar.gz ../artifacts
+    mv "${package}"/*.src.tar.zst ../artifacts
     unset package
 done
 
