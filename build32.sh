41589475177e74afe1f04428420267ca4b61ca71
jeremyd2019 <github@jdrake.com>
2025-09-05 14:56:58 -0700

    Update build32.sh
    add zstd compression options

--- build32.sh
+++ build32.sh
@@ -8,6 +8,8 @@ mkdir ../artifacts
 git_config user.email 'ci@msys2.org'
 git_config user.name  'MSYS2 Continuous Integration'
 [ ! -e ~/.gnupg/gpg.conf ] && mkdir -p ~/.gnupg && echo -e "keyserver keyserver.ubuntu.com\nkeyserver-options auto-key-retrieve" > ~/.gnupg/gpg.conf
+mkdir -p /etc/makepkg.conf.d
+echo "COMPRESSZST=(zstd -c -T0 --ultra -22 -)" > /etc/makepkg.conf.d/zstdopts.conf
 packages=( "$@" )
 
 test -z "${packages}" && success 'No packages - no-op'
